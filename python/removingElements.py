7e2a82ab413b22fff528609c618192b9a398ea97
33patburke <33patburke@gmail.com>
2022-08-30 23:22:56 -0400

    another easy money exercise, spent a while trying to get an in place solution until I remembered that strings are IMMUTABLE in python, and thus this solution is actually optimal

--- python/removingElements.py
+++
@@ -1,9 +0,0 @@
-ex = ["Keep", "Remove", "Keep", "Remove", "Keep"]
-
-def remove_every_other(my_list):
-    for i in range(len(my_list)):
-        if i % 2 != 0:
-            my_list.pop(i)
-    return my_list
-
-remove_every_other(ex)
