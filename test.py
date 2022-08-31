b80ce61c53dabb101436b7646122f49e4050f0b7
33patburke <33patburke@gmail.com>
2022-08-31 00:40:09 -0400

    just fuckin botched the hackerrank at JPM fuck

--- test.py
+++ test.py
@@ -1,11 +1,2 @@
-ex1 = "45385593107843568"
-
-def fake_bin(x):
-    for i in range(len(x)):
-        if x[i] < '5':
-            x = x.replace(x[i], "0", 1)
-        else:
-            x = x.replace(x[i], "1", 1)
-    return x
-
-fake_bin(ex1)
+g = 1
+print(g)
